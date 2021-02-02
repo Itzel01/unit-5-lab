d0348b3844394aee9cd320b7b3dd3eadc37966b9
Asif <kasif5782@gmail.com>
2021-02-02 20:26:51 +0000

    ak

--- index.js
+++ index.js
@@ -1,46 +1,32 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-//let gameActive = true;
+
 let currentPlayer = "X";
 let playerMoves = [];
-let gameActive = true;
-// const statusDisplay = document.querySelector('.game--status');
+let gameActive = false;
+
 
 
 // if the currentPlayer is clicked change color to white : if currentPlayer is clicked change the color to red
 // if td is pressed change the color
 function clickedBlock(clicked){
-
     let clickedBlock = clicked.target;
-    if (currentPlayer === 'X'){
-        clickedBlock.style.backgroundColor = "white";
-        playerMoves.push(clickedBlock);
-        console.log("X:", playerMoves);
-        currentPlayer = 'O';
-    }
-    else {
-        clickedBlock.style.backgroundColor = "red";
-        playerMoves.push(clickedBlock);
-        console.log("O:", playerMoves);
-        currentPlayer = 'X';
-
-    let clickedBlock = clicked.target
     while(!(clickedBlock.style.backgroundColor)){
         if (currentPlayer === 'X'){
             clickedBlock.style.backgroundColor = "white";
-            playerMoves.push(clickedBlock)
-            console.log("X:", playerMoves)
-            currentPlayer = 'O'
+            playerMoves.push(clickedBlock);
+            console.log("X:", playerMoves);
+            currentPlayer = 'O';
         }
         else {
             clickedBlock.style.backgroundColor = "red";
-            playerMoves.push(clickedBlock)
-            console.log("O:", playerMoves)
-            currentPlayer = 'X'
+            playerMoves.push(clickedBlock);
+            console.log("O:", playerMoves);
+            currentPlayer = 'X';
         }
 
-   
+    } 
 }
 
 
@@ -50,49 +36,17 @@ table.forEach(block => block.addEventListener('click', clickedBlock));
 
 
 
-// let resetSubmit = document.querySelector('#button-div')
-// resetSubmit.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     resetSubmit.reset()
-// })
-function resetGame(){
-    document.getElementById('#submit').reset();
+ let button = document.querySelector('button');
+ button.addEventListener('click', resetSubmit);
+ 
+function resetSubmit(){
+
+    if(gameActive == false){
+        currentPlayer = 'x';
+        playerMoves = [];
+        table.forEach(block => block.style.backgroundColor = '');
+    }
 }
-
-// document.getElementById("reset").addEventListener("click", function() {
-//       for (let i = 0; i < playerMoves.length; i++) {
-//         playerMoves[i].innerHTML = "";
-//       }
-//       currentPlayer = " x";
-//       document.getElementById("player").innerHTML = " x";
-//     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
